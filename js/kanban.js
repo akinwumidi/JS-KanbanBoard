51e1eefb39ed439613282c4214cffcc075c8a831
Israel Akinwumi <israelakin14@gmail.com>
2021-12-10 11:49:22 +0800

    Created a new input element and appended it as a child of the item element

--- js/kanban.js
+++ js/kanban.js
@@ -21,7 +21,10 @@ const create_item = () => {
   item.id = 'item-' + order;
   item.draggable = true;
   item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id))
-  item.addEventListener('dragend', event => event.dataTransfer.clearData())
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
+
+  let input = document.createElement('input');
+  item.appendChild(input)
 };
 
 document.querySelectorAll('.drop').forEach(element => {
