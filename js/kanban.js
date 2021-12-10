592f8e0a230ff475655554211931b72c7c186a15
Israel Akinwumi <israelakin14@gmail.com>
2021-12-10 12:40:30 +0800

    Prevented default action when a dragover is initiated

--- js/kanban.js
+++ js/kanban.js
@@ -50,4 +50,8 @@ document.querySelectorAll('.drop').forEach(element => {
     const id = event.dataTransfer.getData('text')
     event.target.appendChild(document.getElementById(id));
   })
+
+  element.addEventListener('dragover', event => {
+    event.preventDefault();
+  })
 });
